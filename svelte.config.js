c080b04e44d0601eca04555b8467b44eb3bc88dc
Ariel <117614710+RielDreams@users.noreply.github.com>
2023-03-01 22:49:14 -0500

    added node

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,12 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
  
 const dev = process.argv.includes('dev');
  
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      // default options are shown. On some platforms
-      // these options are set automatically — see below
-      pages: 'build',
-      assets: 'build',
-      fallback: null,
-      precompress: false,
-      strict: true
-    })
+    adapter: adapter(),
   },
 };
 
