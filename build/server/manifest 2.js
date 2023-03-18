4022d39c1539f42a75798b8e733e992d44b0a7d6
Ariel <117614710+RielDreams@users.noreply.github.com>
2023-03-17 21:51:34 -0400

    added bloomboards live custom link

---
+++ build/server/manifest 2.js
@@ -0,0 +1,23 @@
+const manifest = {
+	appDir: "_app",
+	appPath: "_app",
+	assets: new Set([".nojekyll","CNAME","favicon.png"]),
+	mimeTypes: {".png":"image/png"},
+	_: {
+		entry: {"file":"_app/immutable/start-681ad50b.js","imports":["_app/immutable/start-681ad50b.js","_app/immutable/chunks/index-d9958005.js","_app/immutable/chunks/singletons-2a5c6992.js"],"stylesheets":[],"fonts":[]},
+		nodes: [
+			() => import('./chunks/0-789a6584.js'),
+			() => import('./chunks/1-b7ecf9e7.js')
+		],
+		routes: [
+			
+		],
+		matchers: async () => {
+			
+			return {  };
+		}
+	}
+};
+
+export { manifest };
+//# sourceMappingURL=manifest.js.map
